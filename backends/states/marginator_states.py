ba87cc9bfe6d680ca3aa50f0cdfc8c804c468e65
andreyka140793-create <andreyka140793@gmail.com>
2026-08-25 08:08:06 +0300

    Delete backends directory

--- backends/states/marginator_states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class CalcState(StatesGroup):
-    select_mode = State()         # Выбор режима (Маркетплейс / B2B)
-    upload_file = State()         # Ожидание загрузки файла
-    input_commission = State()    # Ввод комиссии маркетплейса (%)
-    input_logistics = State()     # Ввод стоимости логистики на единицу (₽)
-    input_tax = State()           # Ввод налоговой ставки (%)
-    confirm_params = State()      # Подтверждение и запуск расчета
