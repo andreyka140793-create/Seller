5a4ddbfc14229b36b8662cbf68189d82ead0d725
andreyka140793-create <andreyka140793@gmail.com>
2026-08-26 18:15:12 +0300

    Add files via upload

--- backend/states/marginator_states.py
+++ backend/states/marginator_states.py
@@ -4,9 +4,9 @@ from aiogram.fsm.state import State, StatesGroup
 class CalcState(StatesGroup):
     select_mode = State()
     upload_file = State()
-    confirm_mapping = State()       # подтвердить/править колонки
-    map_pick_field = State()        # какое поле меняем
-    map_pick_column = State()       # выбор колонки из файла
+    confirm_mapping = State()
+    map_pick_field = State()
+    map_pick_column = State()
     select_price_col = State()
     input_commission = State()
     input_logistics = State()
@@ -14,9 +14,9 @@ class CalcState(StatesGroup):
     input_tax = State()
     input_freight = State()
     input_manager_bonus = State()
+    input_fx_rate = State()          # курс: USD 95 / 95 / CNY 13
     input_target_margin = State()
     confirm_params = State()
     save_preset_name = State()
-    # сравнение двух прайсов
     compare_upload_a = State()
     compare_upload_b = State()
